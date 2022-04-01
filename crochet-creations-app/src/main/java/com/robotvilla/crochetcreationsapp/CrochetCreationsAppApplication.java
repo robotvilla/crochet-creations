28c2f5fac94c5da02ccb4abbf6e30bb84339911c
Robert Villazana <r.villazana@yahoo.com>
2022-03-31 21:53:54 -0500

    initial commit

---
+++ crochet-creations-app/src/main/java/com/robotvilla/crochetcreationsapp/CrochetCreationsAppApplication.java
@@ -0,0 +1,13 @@
+package com.robotvilla.crochetcreationsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetCreationsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrochetCreationsAppApplication.class, args);
+	}
+
+}
---
+++ crochet-creations-app/src/test/java/com/robotvilla/crochetcreationsapp/CrochetCreationsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotvilla.crochetcreationsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetCreationsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
